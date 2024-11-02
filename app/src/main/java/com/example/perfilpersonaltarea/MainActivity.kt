601786a264447925dc58e1d773f4143312d92361
Iván <ivan.alonso.munozrepiso@iesjulianmarias.es>
2024-11-02 19:57:12 +0100

    Parte1Casi

--- app/src/main/java/com/example/perfilpersonaltarea/MainActivity.kt
+++ app/src/main/java/com/example/perfilpersonaltarea/MainActivity.kt
@@ -1,16 +1,28 @@
 package com.example.perfilpersonaltarea
-
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        val spinner_paises = findViewById<Spinner>(R.id.spinner)
+
+        val adapter = ArrayAdapter.createFromResource(
+            this,
+            R.array.opciones_paises, R.layout.spinner_paises
+        )
+
+        adapter.setDropDownViewResource(R.layout.spinner_paises)
+
+        spinner_paises.adapter = adapter
+
 
     }
 }
