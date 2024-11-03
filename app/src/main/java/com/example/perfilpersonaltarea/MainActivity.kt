7c55985951a5dc2bc9d2f993ab889b6452613cc8
Iván <ivan.alonso.munozrepiso@iesjulianmarias.es>
2024-11-03 13:55:54 +0100

    Paises y satisfacción funcionan

--- app/src/main/java/com/example/perfilpersonaltarea/MainActivity.kt
+++ app/src/main/java/com/example/perfilpersonaltarea/MainActivity.kt
@@ -1,16 +1,53 @@
 package com.example.perfilpersonaltarea
 import android.os.Bundle
 import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.CheckBox
+import android.widget.EditText
+import android.widget.RadioGroup
+import android.widget.SeekBar
 import android.widget.Spinner
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.SwitchCompat
 
+private lateinit var nombreEditText: EditText
+private lateinit var apellidoEditText: EditText
+private lateinit var correoEditText: EditText
+private lateinit var generoRadioGroup: RadioGroup
+private lateinit var paisSpinner: Spinner
+private lateinit var lecturaCheckBox: CheckBox
+private lateinit var deporteCheckBox: CheckBox
+private lateinit var musicaCheckBox: CheckBox
+private lateinit var arteCheckBox: CheckBox
+private lateinit var nivelSatisfaccionSeekBar: SeekBar
+private lateinit var nivelSatisfaccionTextView: TextView
+private lateinit var suscripcionSwitch: SwitchCompat
+private lateinit var guardarBoton: Button
+private lateinit var resumenTextView: TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        nombreEditText = findViewById(R.id.editTextNombre)
+        apellidoEditText = findViewById(R.id.editTextApellido)
+        correoEditText = findViewById(R.id.editTextCorreo)
+        generoRadioGroup = findViewById(R.id.radioGroupGenero)
+        paisSpinner = findViewById(R.id.spinnerPais)
+        lecturaCheckBox = findViewById(R.id.checkButtonLectura)
+        deporteCheckBox = findViewById(R.id.checkButtonDeporte)
+        musicaCheckBox = findViewById(R.id.checkButtonMusica)
+        arteCheckBox = findViewById(R.id.checkButtonArte)
+        nivelSatisfaccionSeekBar = findViewById(R.id.seekBarSatisfaccion)
+        nivelSatisfaccionTextView = findViewById(R.id.textViewSatisfaccion)
+        suscripcionSwitch = findViewById(R.id.switchSuscripcion)
+        guardarBoton = findViewById(R.id.buttonGuardar)
+        resumenTextView = findViewById(R.id.textViewResumen)
+
 
         val spinnerPaises = findViewById<Spinner>(R.id.spinnerPais)
 
@@ -19,16 +56,39 @@ class MainActivity : AppCompatActivity() {
             R.array.opciones_paises, R.layout.tamanoletra_spinner
         )
         // El layout que pasamos aquí controla el estilo de letra del elemento seleccionado
+        /* Como alt al adaptador que he creado podría haber hecho:
+        val paises = arrayOf("España", "Estados Unidos", "México", "Argentina", "Colombia")
+        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, paises)
+         */
 
         adaptador.setDropDownViewResource(R.layout.tamanoletra_spinner)
         // Este controla el estilo de letra cuando despliego el spinner, en este caso lo mismo.
 
         spinnerPaises.adapter = adaptador
 
+        nivelSatisfaccionSeekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                nivelSatisfaccionTextView.setText(getString(R.string.satisfaccion_dinamica, progress))
+                /* Podría haber concatenado
+                    nivelSatisfaccionTextView.setText(getString(R.string.satisfaccion_dinamica) + progress)
+                    pero es mucho mejor en el xml poner el %d y aquí pasar el argumento dentro de
+                    getString como he hecho.
+                 */
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {}
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {}
+
+        })
+
+
+
+
     }
 
 
-    fun muestraDatos(){
+    private fun onClickGenerate(){
 
 
 
